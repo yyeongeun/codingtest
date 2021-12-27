5242eda39fd466dcd2c78443a988f5c18bc47737
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2021-12-27 19:27:54 +0900

    211227

--- BAEKJOON/2501_약수구하기.py
+++ BAEKJOON/2501_약수구하기.py
@@ -1,5 +1,6 @@
 # 약수 구하기
 ## https://www.acmicpc.net/problem/2501
+### try 2 times
 
 answer = []
 N, k = list(map(int,input().split()))
@@ -8,9 +9,10 @@ for i in range(1,N+1):
     if N % i == 0:
         answer.append(i)
 
-# answer.sort() -> 굳이 할 필요 없다.
-if len(answer) < k:
-    result = 0
+answer.sort() # 이미 값은 sort되어있으므로 굳이 할 필요 없다. 
+
+if len(answer) < k: # len(answer)를 변수로 지정해도 된다.
+    result = 0  # 결과 값을 바로 print 해도 된다.
 else:
     result = answer[k-1]
 
