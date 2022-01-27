5901ff90da89f6824c376edea611172a2c8eb814
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2022-01-27 14:11:18 +0900

    220127

---
+++ BAEKJOON/8958_OX퀴즈.py
@@ -0,0 +1,18 @@
+# 8958번 OX퀴즈
+# https://www.acmicpc.net/problem/8958
+
+n = int(input())
+
+for _ in range(n):
+  ox_list = list(input())
+  score = 0
+  sum = 0
+  
+  for ox in ox_list:
+    if ox == 'O':
+      score += 1
+      sum += score
+    else:
+      scroe = 0
+      
+  print(sum)
