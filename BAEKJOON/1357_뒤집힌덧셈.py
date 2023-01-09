06a4164913d297eb6a9d7cf8ecfcf287c7cd13ce
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-01-09 18:06:42 +0900

    230109

---
+++ BAEKJOON/1357_뒤집힌덧셈.py
@@ -0,0 +1,22 @@
+#내 정답
+X,Y = input().split()
+
+dev Rev(X):
+  ans = 0
+  x = 1
+  for i in range(len(X)):
+    xx = int(x[i])*x
+    ans += xx
+    x *= 10
+  return ans
+
+print(Rev(str(Rev(X)+Rev(Y))))
+
+#보통 정답
+X,Y = input().split()
+
+def Rev(X):
+  ans = int(X[::-1])
+  return ans
+
+print(Rev(str(Rev(X)+Rev(Y))))
