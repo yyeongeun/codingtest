50daf08303c241bc8ab0a1e06e1c165d89565bc1
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-01-16 17:34:37 +0900

    230116

---
+++ BAEKJOON/1406_에디터.py
@@ -0,0 +1,20 @@
+#1406번
+#https://www.acmicpc.net/problem/1406
+
+stack_l = list(input())
+stack_r = []
+n = int(input())
+
+for i in range(n):
+  command = sys.stdin.readline().split()
+  
+  if command[0]=="L" and stack_l:
+    stack_r.append(stack_l.pop())
+  elif command[0] == "D" and stack_r:
+    stack_l.append(stack_r.pop())
+  elif command[0] == "B" and stack_l:
+    stack_l.pop()
+  elif command[0] == "P":
+    stack_l.append(command[1])
+
+print("".join(stack_l+list(reverserd(stack_r))))
