b5b974a61ca9bf8372b63c9dbc58f119f1aac03a
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-01-31 15:35:05 +0900

    230131

---
+++ programmers/게임맵최단거리.py
@@ -0,0 +1,36 @@
+# 프로그래머스 lv 2
+# 게임 맵 최단거리
+# https://school.programmers.co.kr/learn/courses/30/lessons/1844
+
+from collections import deque
+def solution(maps):
+    answer = 0
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    def bfs(x, y):
+        queue = deque()
+        queue.append((x, y))
+        
+        while queue:
+            x, y = queue.popleft()
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]): 
+                    continue
+
+                if maps[nx][ny] == 0:  
+                    continue
+
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    queue.append((nx,ny))
+                    
+        return maps[len(maps)-1][len(maps[0])-1]
+    
+    answer = bfs(0,0)
+    return -1 if answer == 1 else answer
