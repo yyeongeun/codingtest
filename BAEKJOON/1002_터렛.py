d908b5c26982eed0101109248f02627cb52fed71
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-04-07 16:26:30 +0900

    230407

---
+++ BAEKJOON/1002_터렛.py
@@ -0,0 +1,18 @@
+# 백준 1002번 터렛
+# https://www.acmicpc.net/problem/1002
+
+import math
+t = int(input())
+
+for _ in range(t):
+    x1, y1, r1, x2, y2, r2 = map(int,input().split())
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    
+    if distance == 0 and r1 == r2:
+        print(-1)
+    elif abs(r1-r2) == distance or r1 + r2 == distance:
+        print(1)
+    elif abs(r1-r2) < distance < (r1+r2):
+        print(2)
+    else:
+        print(0)
