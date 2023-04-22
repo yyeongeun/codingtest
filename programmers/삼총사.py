44d721bda63b77cdd72f28cf76ac334fcd6495c7
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-04-22 22:53:29 +0900

    230422

---
+++ programmers/삼총사.py
@@ -0,0 +1,14 @@
+# 프로그래머스 lv1 삼총사
+# https://school.programmers.co.kr/learn/courses/30/lessons/131705
+
+def solution(number):
+    answer = 0
+    
+    for i in range(len(number)-2):
+        for j in range(i + 1, len(number)-1):
+            for k in range(j + 1, len(number)):
+                
+                if number[i] + number[j] + number[k] == 0:
+                    answer += 1
+                    
+    return answer
