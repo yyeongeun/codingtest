6612cf1799c60d1ef3481d548894d98bcd38c628
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-05-16 09:23:26 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,23 +1,22 @@
-# defaultdict = dict 입력형식 지정 set or int
 from collections import defaultdict
 
 def solution(id_list,report,k):
     answer = []
     
-    report = list(set(report)) #report 중복제거
-    user = defaultdict(set) #신고한 id : 신고받은 id
-    cnt = defaultdict(int) #신고한 사람별 신고 횟수
+    report = list(set(report))
+    user = defaultdict(set)
+    cnt = defaultdict(int)
     
     for r in report:
-        a,b = r.split() #신고 한 사람, 신고 받은 사람
-        user[a].add(b) # {신고한 사람 : 신고 받은 사람들}
-        cnt[b] += 1 # {신고받은 사람 : 신고 받은 횟수}
+        a,b = r.split()
+        user[a].add(b)
+        cnt[b] += 1
         
     for i in id_list:
         result = 0
-        for u in user[i]: #id별
-            if cnt[u] >= k: #id별 신고횟수 조사
+        for u in user[i]:
+            if cnt[u] >= k:
                 result += 1
         answer.append(result)
-        
+
     return answer
