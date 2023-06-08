419cf8bd52a559751fc5dd25df96cde84f040db2
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-06-08 13:36:22 +0900

    230608

---
+++ 프로그래머스 MYSQL/여러기준으로정렬하기.sql
@@ -0,0 +1,6 @@
+-- 프로그래머스 lv 1 여러 기준으로 정렬하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59404
+
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME ASC, DATETIME DESC
