82dbed99d7757b951959b580bdd1228d49f1bebb
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-06-19 17:30:52 +0900

    230619

---
+++ 프로그래머스 MYSQL/자동차대여기록에서대여중대여가능여부구분하기.sql
@@ -0,0 +1,14 @@
+-- 프로그래머스 LV 3 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/157340
+
+SELECT CAR_ID,
+(CASE WHEN CAR_ID IN (
+    SELECT CAR_ID
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    WHERE '2022-10-16' BETWEEN DATE_FORMAT(START_DATE, '%Y-%m-%d') AND DATE_FORMAT(END_DATE, '%Y-%m-%d'))
+THEN '대여중'
+ELSE '대여 가능'
+END) AS 'AVAILABILITY'
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC
