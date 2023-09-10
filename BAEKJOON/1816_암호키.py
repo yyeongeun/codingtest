e7068cd515f6c9765c5b66db8873882d4a2846d2
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-09-10 13:24:33 +0900

    Create 1816_암호키.py

---
+++ BAEKJOON/1816_암호키.py
@@ -0,0 +1,9 @@
+N = int(input())
+for _ in range(N):
+    S = int(input())
+    for i in range(2,1000001):
+        if S%i == 0:
+            print("NO")
+            break
+        if i == 1000000:
+            print("YES")
