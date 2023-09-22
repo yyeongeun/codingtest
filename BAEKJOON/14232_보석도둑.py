8b39e065b6ac56bfec331c9dffaf5d0bf99bf7be
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-09-22 10:11:12 +0900

    230922

---
+++ BAEKJOON/14232_보석도둑.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/14232
+# 백준 14232번 보석 도둑
+
+n = int(input())
+answer = []
+for i in range(2, int(n**0.5)+1):
+    while n%i == 0:
+        answer.append(i)
+        n //= i
+if n != 1:
+    answer.append(n)    
+    
+print(len(answer))
+print(*answer)
