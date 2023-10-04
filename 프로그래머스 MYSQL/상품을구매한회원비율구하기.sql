68e599e69a9f97ccb441374d487f73b75573fde5
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-10-04 15:29:33 +0900

    231004

---
+++ 프로그래머스 MYSQL/상품을구매한회원비율구하기.sql
@@ -0,0 +1,15 @@
+-- 프로그래머스 lv 4 상품을 구매한 회원 비율 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131534
+
+SELECT YEAR, MONTH, COUNT(*) AS PUCHASED_USERS,
+ROUND((COUNT(*)/(SELECT COUNT(*)
+                 FROM USER_INFO
+                 WHERE YEAR(JOINED) = 2021)),1) AS PUCHASED_RATIO
+
+FROM (SELECT DISTINCT YEAR(S.SALES_DATE) AS YEAR, MONTH(S.SALES_DATE) AS MONTH, U.USER_ID
+      FROM ONLINE_SALE AS S
+      JOIN USER_INFO AS U
+      ON S.USER_ID = U.USER_ID AND YEAR(JOINED) = 2021
+     ) AS A
+GROUP BY YEAR, MONTH
+ORDER BY YEAR, MONTH;
