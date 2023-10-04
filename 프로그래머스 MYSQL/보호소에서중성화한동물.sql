b0541ace49237dea9db47bac3aae4bd061f60afb
YeongEun KIM <70632327+yyeongeun@users.noreply.github.com>
2023-10-04 17:29:39 +0900

    231004

---
+++ 프로그래머스 MYSQL/보호소에서중성화한동물.sql
@@ -0,0 +1,9 @@
+-- 프로그래머스 lv 4 보호소에서 중성화한 동물
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59045
+
+SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
+FROM ANIMAL_INS AS I
+JOIN ANIMAL_OUTS AS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.SEX_UPON_INTAKE != O.SEX_UPON_OUTCOME
+ORDER BY I.ANIMAL_ID;
