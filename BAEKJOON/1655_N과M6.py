2adf86015d318bf8960349b7b5a864152821c06f
hanni K <70632327+yyeongeun@users.noreply.github.com>
2023-10-30 14:40:31 +0900

    231030

---
+++ BAEKJOON/1655_N과M6.py
@@ -0,0 +1,19 @@
+# 백준 N과 M(6)
+# https://www.acmicpc.net/problem/15655
+
+def recur(number):
+    if len(arr) == m:
+        print(*arr)
+        return
+    for i in range(number,n):
+        if list_n[i] not in arr:
+            arr.append(list_n[i])
+            recur(i+1)
+            arr.pop()
+
+        
+n, m = map(int,input().split())
+list_n = list(map(int,input().split()))
+list_n.sort()
+arr = []
+recur(0)
