f43a80256fbf2c9c0e4dcc2f4cd6e767c0256f97
hanni K <70632327+yyeongeun@users.noreply.github.com>
2023-11-01 17:26:36 +0900

    231101

---
+++ BAEKJOON/9251_LCS.py
@@ -0,0 +1,16 @@
+# 백준 9251번 LCS
+# https://www.acmicpc.net/problem/9251
+
+m = list(str(input()))
+n = list(str(input()))
+
+dp = [[0 for _ in range(len(n)+1)] for _ in range(len(m)+1)]
+
+for i in range(1,len(m)+1):
+    for j in range(1,len(n)+1):
+        if m[i-1] == n[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[len(m)][len(n)])
